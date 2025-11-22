0eb90f97d248db872aa04e87e53a10d1f5d5c040
BraedonHaensel <bhaensel111@gmail.com>
2025-11-22 16:22:06 -0700

    added house icon to home button

--- frontend/src/components/home-button.tsx
+++ frontend/src/components/home-button.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Home } from 'lucide-react';
 import Link from 'next/link';
 
 // BarkBase homepage button
@@ -9,7 +10,9 @@ const HomeButton = () => {
       href={'/'}
       className="block rounded-lg border-2 border-r-4 border-b-4 border-black px-2 py-1 hover:opacity-60"
     >
-      <p className="text-xl font-bold">BarkBase</p>
+      <div className="flex items-center gap-2">
+        <Home strokeWidth={2.3} /> <p className="text-xl font-bold">BarkBase</p>
+      </div>
     </Link>
   );
 };
